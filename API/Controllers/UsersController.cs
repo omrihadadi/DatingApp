de0af06996cbe5b3eaaca8aaf519009ea7579e0b
omrihadadi <omrihadadi47@gmail.com>
2023-05-17 14:54:48 +0300

    End Of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // /api/users
+    [Authorize]
 
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         
         private readonly DataContext _context;
@@ -18,6 +18,8 @@ namespace API.Controllers
             _context = context;
         }
 
+
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
    constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5010/api/Users').subscribe({
+    this.http.get('https://localhost:5001/api/Users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
