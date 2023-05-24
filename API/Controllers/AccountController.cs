24ee9fcbdbe5cf75cbc356c6a1d88b423a7871b8
omrihadadi <omrihadadi47@gmail.com>
2023-05-24 12:09:22 +0300

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -41,7 +41,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         } 
 
@@ -49,7 +50,10 @@ namespace API.Controllers
 
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x =>
+             x.UserName == loginDto.Username);
 
             if(user ==null) return Unauthorized("invalid username");
 
@@ -64,7 +68,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }        
 
---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,12 @@
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+        
+    }
+}
